c26a8ea4692e1f6e0f75645068c7242d4c70fc7e
Tyler Bradley <tyler.bradley@phila.gov>
2018-09-28 10:49:07 -0400

    changed function name and made multiple groupings possible.

--- R/rolling-slope.R
+++ R/rolling-slope.R
@@ -3,32 +3,32 @@
 #' @param data a dataframe or tbl that contains at the minimum a date column
 #' and a chlorine residual column
 #' @param date_col the unqouted name of the date column
-#' @param chlorine_col the unquoted name of the chlorine residual column
-#' @param group_col the unqouted name of a grouping column, if grouping is
-#' needed - e.g. if the dataset contains multiple sites.
+#' @param value_col the unquoted name of the chlorine residual column
+#' @param ... the unqouted names of all grouping columns, if grouping is
+#' needed - e.g. if the dataset contains multiple sites and/or parameters.
 #' @param rolling_window how large should the rolling mean window be?
 #' @importFrom stats smooth.spline predict
-find_slope <- function(data, date_col, chlorine_col, group_col = NULL, rolling_window = 8){
+rolling_slope <- function(data, date_col, value_col, ..., rolling_window = 8){
   if (!"data.frame" %in% class(data)) stop("data must be a data.frame or a tibble")
 
   date_col <- rlang::enquo(date_col)
   date_name <- rlang::quo_name(date_col)
-  chlorine_col <- rlang::enquo(chlorine_col)
-  chlorine_name <- rlang::quo_name(chlorine_col)
+  value_col <- rlang::enquo(value_col)
+  value_name <- rlang::quo_name(value_col)
+  group_cols <- rlang::quos(...)
 
-  if (!is.null(group_col)) {
-    group_col <- rlang::enquo(group_col)
-    data <- dplyr::group_by(data, !!group_col)
+  if (!rlang::is_empty(group_col)) {
+    data <- dplyr::group_by(data, !!!group_col)
   }
 
   rolling_mean <- tibbletime::rollify(mean, rolling_window)
 
   output <- data %>%
-    dplyr::filter(!is.na(!!chlorine_col),
+    dplyr::filter(!is.na(!!value_col),
                   !is.na(!!date_col)) %>%
     tidyr::nest() %>%
     dplyr::mutate(
-      spline = purrr::map(.data$data, ~smooth.spline(.x[[ !!date_name ]], .x[[ !!chlorine_name ]])),
+      spline = purrr::map(.data$data, ~smooth.spline(.x[[ !!date_name ]], .x[[ !!value_name ]])),
       first_deriv = purrr::map2(.data$spline, .data$data, ~{
         predict(.x, as.numeric(.y[[ !!date_name ]]), deriv = 1) %>%
           tibble::as_tibble()
@@ -39,7 +39,7 @@ find_slope <- function(data, date_col, chlorine_col, group_col = NULL, rolling_w
           dplyr::left_join(.y, by = c("date_numeric" = "x")) %>%
           dplyr::rename(first_deriv = y) %>%
           dplyr::mutate(
-            roll_mean = rolling_mean(!!chlorine_col)
+            roll_mean = rolling_mean(!!value_col)
           ) %>%
           dplyr::filter(!is.na(roll_mean))
       }),

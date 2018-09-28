7d57ecc743176e7892bba61e902bd2c79bf2a696
Tyler Bradley <tyler.bradley@phila.gov>
2018-09-28 08:11:47 -0400

    add function to calculate first and second derivative of chlorine time series

---
+++ R/find-slope.R
@@ -0,0 +1,81 @@
+#' calculate first and second derivative of chlorine trend
+#'
+#' @param data a dataframe or tbl that contains at the minimum a date column
+#' and a chlorine residual column
+#' @param date_col the unqouted name of the date column
+#' @param chlorine_col the unquoted name of the chlorine residual column
+#' @param group_col the unqouted name of a grouping column, if grouping is
+#' needed - e.g. if the dataset contains multiple sites.
+#' @param rolling_window how large should the rolling mean window be?
+#' @importFrom stats smooth.spline predict
+find_slope <- function(data, date_col, chlorine_col, group_col = NULL, rolling_window = 8){
+  if (!"data.frame" %in% class(data)) stop("data must be a data.frame or a tibble")
+
+  date_col <- rlang::enquo(date_col)
+  date_name <- rlang::quo_name(date_col)
+  chlorine_col <- rlang::enquo(chlorine_col)
+  chlorine_name <- rlang::quo_name(chlorine_col)
+
+  if (!is.null(group_col)) {
+    group_col <- rlang::enquo(group_col)
+    data <- dplyr::group_by(data, !!group_col)
+  }
+
+  rolling_mean <- tibbletime::rollify(mean, rolling_window)
+
+  output <- data %>%
+    dplyr::filter(!is.na(!!chlorine_col),
+                  !is.na(!!date_col)) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(
+      spline = purrr::map(.data$data, ~smooth.spline(.x[[ !!date_name ]], .x[[ !!chlorine_name ]])),
+      first_deriv = purrr::map2(.data$spline, .data$data, ~{
+        predict(.x, as.numeric(.y[[ !!date_name ]]), deriv = 1) %>%
+          tibble::as_tibble()
+      }),
+      data_new = purrr::map2(.data$data, .data$first_deriv, ~{
+        .x %>%
+          dplyr::mutate(date_numeric = as.numeric(!!date_col)) %>%
+          dplyr::left_join(.y, by = c("date_numeric" = "x")) %>%
+          dplyr::rename(first_deriv = y) %>%
+          dplyr::mutate(
+            roll_mean = rolling_mean(!!chlorine_col)
+          ) %>%
+          dplyr::filter(!is.na(roll_mean))
+      }),
+      spline_ma = purrr::map(.data$data_new, ~smooth.spline(.x[[ !!date_name ]], .x$roll_mean)),
+      first_deriv_ma = purrr::map2(.data$spline_ma, .data$data_new, ~{
+        predict(.x, .y$date_numeric, deriv = 1) %>%
+          tibble::as_tibble()
+      }),
+      deriv_spline_ma = purrr::map2(.data$first_deriv_ma, .data$data_new, ~{
+        smooth.spline(.y[[ !!date_name ]], .x$y)
+      }),
+      second_deriv_ma = purrr::map2(.data$deriv_spline_ma, .data$data_new, ~{
+        predict(.x, .y$date_numeric, deriv = 1) %>%
+          tibble::as_tibble()
+      }),
+      data_new = purrr::pmap(
+        .l = list(
+          .data$data_new,
+          .data$first_deriv_ma,
+          .data$second_deriv_ma
+        ),
+        .f = ~{
+          ..1 %>%
+            dplyr::left_join(..2, by = c("date_numeric" = "x")) %>%
+            dplyr::rename(first_deriv_ma = y) %>%
+            dplyr::left_join(..3, by = c("date_numeric" = "x")) %>%
+            dplyr::rename(second_deriv_ma = y)
+        }
+      )
+    ) %>%
+    tidyr::unnest(data_new) %>%
+    dplyr::mutate(
+      rolling_first = rolling_mean(first_deriv_ma),
+      rolling_second = rolling_mean(second_deriv_ma)
+    )
+
+    return(output)
+
+}

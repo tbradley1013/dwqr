a32c150b2d951f15a12e35c90d7a6c597b49f0f6
Tyler Bradley <tyler.bradley@phila.gov>
2018-11-02 09:36:50 -0400

    add option for users to pass explicit action levels rather than calculating them

--- R/plot-al.R
+++ R/plot-al.R
@@ -26,12 +26,19 @@
 #' @param ylab a character string specifying the y axis label for the main plot
 #' @param plot_title a character string specifying the title of the plot
 #' @param plot_subtitle a chacter string specifying the subtitle of the plot
+#' @param action_levels a numeric vector specifying action levels. If this is
+#' NULL (default) than the action levels will be specified via the method
+#' provided in the method arguement. If this argument is given, then the vector
+#' will be sorted with the lowest number by default being the highest action level
+#' and the highest number given being the lowest action level. The number of action
+#' levels in the resulting data will be equal to the number of action levels given
 #'
 #' @export
 plot_al <- function(data, date_col, value_col, ..., method = c("FL", "P"),
                     percentiles = c(.8, .5, .1), rolling_window = 8,
                     max_chlorine = 1.5, date_breaks = "6 months", date_labels = "%b %d, %Y",
-                    ylab = "", plot_title = "", plot_subtitle = ""){
+                    ylab = "", plot_title = "", plot_subtitle = "",
+                    action_levels = NULL){
 
   if (!"data.frame" %in% class(data)) stop("data must be of class data.frame or tbl")
   req_cols <- c(missing(date_col), missing(value_col))
@@ -56,62 +63,115 @@ plot_al <- function(data, date_col, value_col, ..., method = c("FL", "P"),
     purrr::set_names(nm = quant_names)
 
 
+  if (!is.null(action_levels)){
+    if (!is.numeric(action_levels)) stop("action_levels must be a numeric vector")
 
-  if (method == "FL") {
-    data_classed <- data %>%
-      rolling_slope(!!date_col, !!value_col, ..., rolling_window = rolling_window) %>%
-      falling_limb(!!value_col, rolling_first, rolling_second, ..., max_chlorine = max_chlorine)
+    action_levels <- sort(action_levels, decreasing = TRUE)
 
-    if (!rlang::is_empty(group_cols)) {
-      data_classed <- dplyr::group_by(data_classed, !!!group_cols)
+    names(action_levels) <- paste0("action_level", 1:length(action_levels))
+
+    action_levels <- sort(action_levels)
+
+    p <- data %>%
+      dplyr::mutate(
+        level = purrr::map_chr(!!value_col, function(y){
+          output <- purrr::map(action_levels, ~{
+            if (y < .x) return(.x)
+          }) %>%
+            purrr::flatten() %>%
+            .[. == min(purrr::flatten_dbl(.))] %>%
+            names()
+
+          if (is.null(output)){
+            return("No Action Required")
+          } else return(output)
+        })
+      ) %>%
+      ggplot2::ggplot(ggplot2::aes(!!date_col, !!value_col, color = level)) +
+      ggplot2::geom_point() +
+      ggplot2::facet_wrap(dplyr::vars(!!!group_vars)) +
+      ggplot2::theme_bw() +
+      ggplot2::scale_color_manual(
+        values = c(
+          "No Action Required" = "black",
+          "Action Level 1" = "#ff9a00",
+          "Action Level 2" = "#ff7400",
+          "Action Level 3" = "#ff0000"
+        ),
+        name = ""
+      ) +
+      ggplot2::labs(
+        y = ylab,
+        title = plot_title,
+        subtitle = plot_subtitle
+      )
+
+    return(p)
+
+  } else {
+
+    if (method == "FL") {
+      data_classed <- data %>%
+        rolling_slope(!!date_col, !!value_col, ..., rolling_window = rolling_window) %>%
+        falling_limb(!!value_col, rolling_first, rolling_second, ..., max_chlorine = max_chlorine)
+
+      if (!rlang::is_empty(group_cols)) {
+        data_classed <- dplyr::group_by(data_classed, !!!group_cols)
+      }
+
+
+
+      plot_data <- data_classed %>%
+        dplyr::filter(falling_limb == "Falling Limb") %>%
+        dplyr::mutate_at(dplyr::vars(!!value_col), dplyr::funs(!!!quants))
+
+    } else if (method == "P") {
+      if (!rlang::is_empty(group_cols)) {
+        data <- dplyr::group_by(data, !!!group_cols)
+      }
+
+      plot_data <- data %>%
+        dplyr::mutate_at(dplyr::vars(!!value_col), dplyr::funs(!!!quants))
+
     }
 
+    p <- plot_data %>%
+      {suppressMessages(dplyr::full_join(., data_classed))} %>%
+      tidyr::fill(dplyr::contains("action_level")) %>%
+      tidyr::fill(dplyr::contains("action_level"), .direction = "up") %>%
+      dplyr::mutate(
+        level = dplyr::case_when(
+          !!value_col < action_level_3 ~ "Action Level 3",
+          !!value_col < action_level_2 ~ "Action Level 2",
+          !!value_col < action_level_1 ~ "Action Level 1",
+          TRUE ~ "No Action Required"
+        )
+      ) %>%
+      ggplot2::ggplot(ggplot2::aes(!!date_col, !!value_col, color = level)) +
+      ggplot2::geom_point() +
+      ggplot2::facet_wrap(dplyr::vars(!!!group_cols)) +
+      ggplot2::theme_bw() +
+      ggplot2::scale_color_manual(
+        values = c(
+          "No Action Required" = "black",
+          "Action Level 1" = "#ff9a00",
+          "Action Level 2" = "#ff7400",
+          "Action Level 3" = "#ff0000"
+        ),
+        name = ""
+      ) +
+      ggplot2::labs(
+        y = ylab,
+        title = plot_title,
+        subtitle = plot_subtitle
+      )
 
-
-    plot_data <- data_classed %>%
-      dplyr::filter(falling_limb == "Falling Limb") %>%
-      dplyr::mutate_at(dplyr::vars(!!value_col), dplyr::funs(!!!quants))
-
-  } else if (method == "P") {
-    if (!rlang::is_empty(group_cols)) {
-      data <- dplyr::group_by(data, !!!group_cols)
-    }
-
-    plot_data <- data %>%
-      dplyr::mutate_at(dplyr::vars(!!value_col), dplyr::funs(!!!quants))
-
+    return(p)
   }
 
-  plot_data %>%
-    {suppressMessages(dplyr::full_join(., data_classed))} %>%
-    tidyr::fill(dplyr::contains("action_level")) %>%
-    tidyr::fill(dplyr::contains("action_level"), .direction = "up") %>%
-    dplyr::mutate(
-      level = dplyr::case_when(
-        !!value_col < action_level_3 ~ "Action Level 3",
-        !!value_col < action_level_2 ~ "Action Level 2",
-        !!value_col < action_level_1 ~ "Action Level 1",
-        TRUE ~ "No Action Required"
-      )
-    ) %>%
-    ggplot2::ggplot(ggplot2::aes(!!date_col, !!value_col, color = level)) +
-    ggplot2::geom_point() +
-    ggplot2::facet_wrap(dplyr::vars(!!!group_cols)) +
-    ggplot2::theme_bw() +
-    ggplot2::scale_color_manual(
-      values = c(
-        "No Action Required" = "black",
-        "Action Level 1" = "#ff9a00",
-        "Action Level 2" = "#ff7400",
-        "Action Level 3" = "#ff0000"
-      ),
-      name = ""
-    ) +
-    ggplot2::labs(
-      y = ylab,
-      title = plot_title,
-      subtitle = plot_subtitle
-    )
+
+
+
 
 
 

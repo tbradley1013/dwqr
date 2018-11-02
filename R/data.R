73df998ad61d0acc5f382d50ad64103f2e71f5f5
Tyler Bradley <tyler.bradley@phila.gov>
2018-11-02 10:33:37 -0400

    added example nitrification dataset

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' \strong{dwqr datasets}
+#' @name dwqr_datasets
+NULL
+
+#' nitrification
+#'
+#' An example dataset in the correct format for use with the \code{dwqr}
+#' nitrification functions (e.g nitrification_al, plot_fl, plot_al)
+#'
+#' @rdname dwqr_datasets
+"nitrification"

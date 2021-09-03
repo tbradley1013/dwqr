914f1af5d93c90d6e0cd2b19a0fe183fa5344a0c
Tyler Bradley <tyler.bradley@phila.gov>
2021-09-03 12:22:31 -0400

    adding simple and hmm classification methods for falling limb

--- R/falling-limb.R
+++ R/falling-limb.R
@@ -19,9 +19,105 @@
 #'
 #'
 #' @export
-falling_limb <- function(data, value_col, first_deriv, second_deriv, ...){
+falling_limb <- function(data, method = c("simple", "hmm"), value_col, first_deriv, second_deriv, ...){
   if (!"data.frame" %in% class(data)) stop("data must be a data.frame or a tibble")
 
+  value_col <- rlang::enquo(value_col)
+  first_deriv <- rlang::enquo(first_deriv)
+  second_deriv <- rlang::enquo(second_deriv)
+  group_cols <- rlang::enquos(...)
+
+  if (method == "simple"){
+    out <- fl_class_simple(data, value_col = !!value_col, first_deriv = !!first_deriv, second_deriv = !!second_deriv, ...)
+  } else if (method == "hmm"){
+    out <- fl_class_hmm(data, value_col = !!value_col, first_deriv = !!first_deriv, second_deriv = !!second_deriv, ...)
+  }
+
+  return(out)
+}
+
+
+fl_class_hmm <- function(data, value_col, first_deriv, second_deriv, ...){
+  first_deriv <- rlang::enquo(first_deriv)
+  group_cols <- rlang::enquos(...)
+
+  if (!rlang::is_empty(group_cols)) {
+    data <- dplyr::group_nest(data, !!!group_cols)
+
+    out <- data %>%
+      dplyr::mutate(
+        data = purrr::map(data, ~{
+          .x <- .x %>%
+            dplyr::mutate(x = !!first_deriv)
+
+          mod <- depmixS4::mix(
+            list(x ~ 1),
+            data = .x,
+            nstates = 2,
+            family = list(stats::gaussian())
+          )
+
+          fm <- depmixS4::fit(mod)
+
+          states <- depmixS4::posterior(fm)
+
+          out <- .x %>%
+            dplyr::mutate(
+              est_state = as.character(states$state)
+            ) %>%
+            dplyr::group_by(est_state) %>%
+            dplyr::mutate(
+              state_avg_deriv = mean(x, na.rm = TRUE)
+            ) %>%
+            dplyr::ungroup() %>%
+            dplyr::mutate(
+              falling_limb = ifelse(state_avg_deriv == min(state_avg_deriv), "Falling Limb", "Other")
+            ) %>%
+            dplyr::select(-c(x, state_avg_deriv, est_state))
+
+          return(out)
+        })
+      ) %>%
+      tidyr::unnest(data)
+  } else {
+    data <- data %>%
+      dplyr::mutate(x = !!first_deriv)
+
+    mod <- depmixS4::mix(
+      list(x ~ 1),
+      data = data,
+      nstates = 2,
+      family = list(stats::gaussian())
+    )
+
+    fm <- depmixS4::fit(mod)
+
+    states <- depmixS4::posterior(fm)
+
+    out <- data %>%
+      dplyr::mutate(
+        est_state = as.character(states$state)
+      ) %>%
+      dplyr::group_by(est_state) %>%
+      dplyr::mutate(
+        state_avg_deriv = mean(x, na.rm = TRUE)
+      ) %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate(
+        falling_limb = ifelse(state_avg_deriv == min(state_avg_deriv), "Falling Limb", "Other")
+      ) %>%
+      dplyr::select(-c(x, state_avg_deriv, est_state))
+  }
+
+
+
+  return(out)
+
+
+}
+
+
+fl_class_simple <- function(data, value_col, first_deriv, second_deriv, ...){
   value_col <- rlang::enquo(value_col)
   first_deriv <- rlang::enquo(first_deriv)
   second_deriv <- rlang::enquo(second_deriv)
@@ -45,14 +141,4 @@ falling_limb <- function(data, value_col, first_deriv, second_deriv, ...){
   }
 
   return(output)
-}
-
-
-fl_class_hmm <- function(x){
-
-}
-
-
-fl_class_manual <- function(x){
-
 }

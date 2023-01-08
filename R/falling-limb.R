59c821af2a74de09e05145807a1dbcca4fd35fbd
Tyler  Bradley <tylerbradley@tylers-mbp.myfiosgateway.com>
2023-01-07 21:30:36 -0500

    switching to new implementation of rlang quosures and adding options for model implementation

--- R/falling-limb.R
+++ R/falling-limb.R
@@ -11,7 +11,7 @@
 #' results for the time series
 #' @param first_deriv unquoted column name of column containing first derivative
 #' of chlorine time series
-#' @param ... unqouted column names of any grouping columns
+#' @param group_col vector of unqouted column names of any grouping columns
 #'
 #' @details
 #' the method argument must be set to one of the following:
@@ -27,97 +27,66 @@
 #'
 #'
 #' @export
-falling_limb <- function(data, method = c("simple", "hmm", "cp"), value_col, first_deriv, ...){
+falling_limb <- function(data, method = c("simple", "hmm", "cp"), value_col, first_deriv, group_col){
   if (!"data.frame" %in% class(data)) stop("data must be a data.frame or a tibble")
 
-  value_col <- rlang::enquo(value_col)
-  first_deriv <- rlang::enquo(first_deriv)
-  group_cols <- rlang::enquos(...)
+  # value_col <- rlang::enquo(value_col)
+  # first_deriv <- rlang::enquo(first_deriv)
+  # group_cols <- rlang::enquos(...)
 
   if (method == "simple"){
-    out <- fl_class_simple(data, value_col = !!value_col, first_deriv = !!first_deriv, ...)
+    out <- fl_class_simple(data, value_col = {{value_col}}, first_deriv = {{first_deriv}}, group_col = {{group_col}})
   } else if (method == "hmm"){
-    out <- fl_class_hmm(data, value_col = !!value_col, first_deriv = !!first_deriv, ...)
+    out <- fl_class_hmm(data, value_col = {{value_col}}, first_deriv = {{first_deriv}}, group_col = {{group_col}})
   } else if (method == "cp"){
-    out <- fl_class_cp(data, value_col = !!value_col, first_deriv = !!first_deriv, ...)
+    out <- fl_class_cp(data, value_col = {{value_col}}, first_deriv = {{first_deriv}}, group_col = {{group_col}})
   }
 
   return(out)
 }
 
 
-fl_class_hmm <- function(data, value_col, first_deriv, ...){
-  first_deriv <- rlang::enquo(first_deriv)
-  group_cols <- rlang::enquos(...)
+fl_class_hmm <- function(data, value_col, first_deriv, group_col){
+  # first_deriv <- rlang::enquo(first_deriv)
+  # group_cols <- rlang::enquos(...)
 
-  if (!rlang::is_empty(group_cols)) {
-    data <- dplyr::group_nest(data, !!!group_cols)
+  data <- dplyr::group_nest(data, dplyr::across({{group_col}}))
 
-    out <- data %>%
-      dplyr::mutate(
-        data = purrr::map(data, ~{
-          .x <- .x %>%
-            dplyr::mutate(x = !!first_deriv)
+  out <- data %>%
+    dplyr::mutate(
+      data = purrr::map(data, ~{
+        .x <- .x %>%
+          dplyr::mutate(x = {{first_deriv}})
 
-          mod <- depmixS4::mix(
-            list(x ~ 1),
-            data = .x,
-            nstates = 2,
-            family = list(stats::gaussian())
-          )
+        mod <- depmixS4::mix(
+          list(x ~ 1),
+          data = .x,
+          nstates = 2,
+          family = list(stats::gaussian())
+        )
 
-          fm <- depmixS4::fit(mod)
+        fm <- depmixS4::fit(mod)
 
-          states <- depmixS4::posterior(fm)
+        states <- depmixS4::posterior(fm)
 
-          out <- .x %>%
-            dplyr::mutate(
-              est_state = as.character(states$state)
-            ) %>%
-            dplyr::group_by(est_state) %>%
-            dplyr::mutate(
-              state_avg_deriv = mean(x, na.rm = TRUE)
-            ) %>%
-            dplyr::ungroup() %>%
-            dplyr::mutate(
-              falling_limb = ifelse(state_avg_deriv == min(state_avg_deriv), "Falling Limb", "Other")
-            ) %>%
-            dplyr::select(-c(x, state_avg_deriv, est_state))
+        out <- .x %>%
+          dplyr::mutate(
+            est_state = as.character(states$state)
+          ) %>%
+          dplyr::group_by(est_state) %>%
+          dplyr::mutate(
+            state_avg_deriv = mean(x, na.rm = TRUE)
+          ) %>%
+          dplyr::ungroup() %>%
+          dplyr::mutate(
+            falling_limb = ifelse(state_avg_deriv == min(state_avg_deriv), "Falling Limb", "Other")
+          ) %>%
+          dplyr::select(-c(x, state_avg_deriv, est_state))
 
-          return(out)
-        })
-      ) %>%
-      tidyr::unnest(data)
-  } else {
-    data <- data %>%
-      dplyr::mutate(x = !!first_deriv)
-
-    mod <- depmixS4::mix(
-      list(x ~ 1),
-      data = data,
-      nstates = 2,
-      family = list(stats::gaussian())
-    )
-
-    fm <- depmixS4::fit(mod)
-
-    states <- depmixS4::posterior(fm)
-
-    out <- data %>%
-      dplyr::mutate(
-        est_state = as.character(states$state)
-      ) %>%
-      dplyr::group_by(est_state) %>%
-      dplyr::mutate(
-        state_avg_deriv = mean(x, na.rm = TRUE)
-      ) %>%
-      dplyr::ungroup() %>%
-      dplyr::mutate(
-        falling_limb = ifelse(state_avg_deriv == min(state_avg_deriv), "Falling Limb", "Other")
-      ) %>%
-      dplyr::select(-c(x, state_avg_deriv, est_state))
-  }
-
+        return(out)
+      })
+    ) %>%
+  tidyr::unnest(data)
 
 
   return(out)
@@ -126,93 +95,67 @@ fl_class_hmm <- function(data, value_col, first_deriv, ...){
 }
 
 
-fl_class_simple <- function(data, value_col, first_deriv, ...){
-  value_col <- rlang::enquo(value_col)
-  first_deriv <- rlang::enquo(first_deriv)
-  group_cols <- rlang::enquos(...)
+fl_class_simple <- function(data, value_col, first_deriv, group_col){
+  # value_col <- rlang::enquo(value_col)
+  # first_deriv <- rlang::enquo(first_deriv)
+  # group_cols <- rlang::enquos(...)
 
-  if (!rlang::is_empty(group_cols)) {
-    data <- dplyr::group_by(data, !!!group_cols)
-  }
+  # if (!is.null(group_col)) {
+  #   data <- dplyr::group_by(data, dplyr::across({{group_col}}))
+  # }
 
 
   output <- data %>%
+    dplyr::group_by(dplyr::across({{group_col}})) %>%
     dplyr::mutate(
       falling_limb = dplyr::case_when(
-        !!first_deriv < 0 & lag(!!first_deriv) < 0 ~ "Falling Limb",
+        {{first_deriv}} < 0 & lag({{first_deriv}}) < 0 ~ "Falling Limb",
         TRUE ~ "Other"
       ),
     )
 
-  if (!rlang::is_empty(group_cols)) {
-    output <- dplyr::ungroup(output)
-  }
+  output <- dplyr::ungroup(output)
 
   return(output)
 }
 
 
-fl_class_cp <- function(data, value_col, first_deriv, ...){
-  value_col <- rlang::enquo(value_col)
-  first_deriv <- rlang::enquo(first_deriv)
-  group_cols <- rlang::enquos(...)
+fl_class_cp <- function(data, value_col, first_deriv, group_col){
+  # value_col <- rlang::enquo(value_col)
+  # first_deriv <- rlang::enquo(first_deriv)
+  # group_cols <- rlang::enquos(...)
 
-  if (!rlang::is_empty(group_cols)) {
-    data <- dplyr::group_nest(data, !!!group_cols)
+  data <- dplyr::group_nest(data, dplyr::across({{group_col}}))
 
-    out <- data %>%
-      dplyr::mutate(
-        data = purrr::map(data, ~{
-          .x <- .x %>%
-            dplyr::mutate(x = !!first_deriv)
+  out <- data %>%
+    dplyr::mutate(
+      data = purrr::map(data, ~{
+        .x <- .x %>%
+          dplyr::mutate(x = {{first_deriv}})
 
-          bps <- strucchange::breakpoints(.x$x ~ 1, breaks = 5)
+        bps <- strucchange::breakpoints(.x$x ~ 1, breaks = 5)
 
-          bp_tbl <- tibble(
-            bp = c(1, bps$breakpoints),
-            bp_num = seq_along(c(1, bps$breakpoints))
-          )
+        bp_tbl <- tibble(
+          bp = c(1, bps$breakpoints),
+          bp_num = seq_along(c(1, bps$breakpoints))
+        )
 
-          out <- .x %>%
-            dplyr::mutate(row = dplyr::row_number()) %>%
-            dplyr::left_join(
-              bp_tbl, by = c("row" = "bp")
-            ) %>%
-            tidyr::fill(bp_num, .direction = "down") %>%
-            dplyr::group_by(bp_num) %>%
-            dplyr::mutate(bp_group_median = median(x, na.rm = TRUE)) %>%
-            dplyr::ungroup() %>%
-            dplyr::mutate(falling_limb = ifelse(bp_group_median < 0, "Falling Limb", "Other")) %>%
-            dplyr::select(-c(x, row, bp_num, bp_group_median))
+        out <- .x %>%
+          dplyr::mutate(row = dplyr::row_number()) %>%
+          dplyr::left_join(
+            bp_tbl, by = c("row" = "bp")
+          ) %>%
+          tidyr::fill(bp_num, .direction = "down") %>%
+          dplyr::group_by(bp_num) %>%
+          dplyr::mutate(bp_group_median = median(x, na.rm = TRUE)) %>%
+          dplyr::ungroup() %>%
+          dplyr::mutate(falling_limb = ifelse(bp_group_median < 0, "Falling Limb", "Other")) %>%
+          dplyr::select(-c(x, row, bp_num, bp_group_median))
 
-          return(out)
-        })
-      ) %>%
-      tidyr::unnest(data)
-  } else {
-    data <- data %>%
-      dplyr::mutate(x = !!first_deriv)
-
-    bps <- strucchange::breakpoints(data$x ~ 1, breaks = 5)
-
-    bp_tbl <- tibble(
-      bp = c(1, bps$breakpoints),
-      bp_num = seq_along(c(1, bps$breakpoints))
-    )
-
-    out <- data %>%
-      dplyr::mutate(row = dplyr::row_number()) %>%
-      dplyr::left_join(
-        bp_tbl, by = c("row" = "bp")
-      ) %>%
-      tidyr::fill(bp_num, .direction = "down") %>%
-      dplyr::group_by(bp_num) %>%
-      dplyr::mutate(bp_group_median = median(x, na.rm = TRUE)) %>%
-      dplyr::ungroup() %>%
-      dplyr::mutate(falling_limb = ifelse(bp_group_median < 0, "Falling Limb", "Other")) %>%
-      dplyr::select(-c(x, row, bp_num, bp_group_median))
-  }
-
+        return(out)
+      })
+    ) %>%
+    tidyr::unnest(data)
 
 
   return(out)
--- R/rolling-slope.R
+++ R/rolling-slope.R
@@ -8,68 +8,86 @@
 #' and a chlorine residual column
 #' @param date_col the unqouted name of the date column
 #' @param value_col the unquoted name of the chlorine residual column
-#' @param ... the unqouted names of all grouping columns, if grouping is
+#' @param group_col vector of unqouted names of all grouping columns, if grouping is
 #' needed - e.g. if the dataset contains multiple sites and/or parameters.
-#' @param rolling_window how large should the rolling mean window be? Specifies
-#' the number of weeks worth of data to include in rolling mean calculations
+#' @param model the model type to use to estimate total chlorine trend. One of
+#' "ss" (smooth spline), "gam" (generalized additive model), or "loess"
+#' (Local Polynomial Regression)
 #' @importFrom stats smooth.spline predict
 #'
 #' @export
-rolling_slope <- function(data, date_col, value_col, ..., rolling_window = 8, return_models = FALSE){
+rolling_slope <- function(data, date_col, value_col, group_col = NULL,
+                          model = c("ss", "gam", "loess"), return_models = FALSE){
   if (!"data.frame" %in% class(data)) stop("data must be a data.frame or a tibble")
 
-  date_col <- rlang::enquo(date_col)
-  date_name <- rlang::quo_name(date_col)
-  value_col <- rlang::enquo(value_col)
-  value_name <- rlang::quo_name(value_col)
-  group_cols <- rlang::enquos(...)
+  # date_col <- rlang::enquo(date_col)
+  date_name <- rlang::as_string(rlang::ensym(date_col))
+  # value_col <- rlang::enquo(value_col)
+  value_name <- rlang::as_string(rlang::ensym(value_col))
+  # group_cols <- rlang::enquos(group_col)
 
   # if (is.null(deriv_window)) deriv_window <- rolling_window
+  # if (!is.null(group_col)) {
+  #   data <- dplyr::group_by(data, dplyr::across({{group_col}}))
+  # }
 
-  if (!rlang::is_empty(group_cols)) {
-    data <- dplyr::group_by(data, !!!group_cols)
+
+  data_nest <- data %>%
+    dplyr::group_by(dplyr::across({{group_col}})) %>%
+    dplyr::filter(!is.na({{value_col}}),
+                  !is.na({{date_col}})) %>%
+    dplyr::mutate(
+      # roll_mean = zoo::rollmean(!!value_col, rolling_window, fill = NA),
+      date_numeric = as.numeric({{date_col}})
+    ) %>%
+    # dplyr::filter(!is.na(roll_mean)) %>%
+    tidyr::nest()
+
+  if (model == "ss"){
+    output <- data_nest %>%
+      mutate(
+        spline = purrr::map(.data$data, ~smooth.spline(.x[[ date_name ]], .x[[ value_name ]])),
+        first_deriv = purrr::map2(.data$spline, .data$data, ~{
+          predict(.x, as.numeric(.y[[ date_name ]]), deriv = 1) %>%
+            tibble::as_tibble()
+        })
+      )
+  } else if (model == "gam") {
+    output <- data_nest %>%
+      mutate(
+        gam_model = purrr::map(data, ~{
+          mgcv::gam(chlorine_residual_total ~ s(date_numeric, bs = "cr", m=2, k = 64), data = .x)
+        }),
+        first_deriv = purrr::map2(gam_model, data, ~{
+          gratia::derivatives(.x, term = "s(date_numeric)", type = "central", n = nrow(.y), eps = 1e-5) %>%
+            dplyr::select(first_deriv_gam = derivative)
+
+        })
+      )
+  } else if (model == "loess") {
+    output <- data_nest %>%
+      mutate(
+        loess_model = purrr::map(data, ~{
+          loess(chlorine_residual_total ~ date_numeric, data = .x, span = .15)
+        }),
+        first_deriv = purrr::map(loess_model, ~{
+          DAMisc::loessDeriv(.x)
+        }),
+      )
   }
 
-
-  output <- data %>%
-    dplyr::filter(!is.na(!!value_col),
-                  !is.na(!!date_col)) %>%
+  output <- output %>%
     dplyr::mutate(
-      roll_mean = zoo::rollmean(!!value_col, rolling_window, fill = NA),
-      date_numeric = as.numeric(!!date_col)
-    ) %>%
-    dplyr::filter(!is.na(roll_mean)) %>%
-    tidyr::nest() %>%
-    dplyr::mutate(
-      spline = purrr::map(.data$data, ~smooth.spline(.x[[ !!date_name ]], .x[[ !!value_name ]])),
-      first_deriv = purrr::map2(.data$spline, .data$data, ~{
-        predict(.x, as.numeric(.y[[ !!date_name ]]), deriv = 1) %>%
-          tibble::as_tibble()
-      }),
-      spline_ma = purrr::map(.data$data, ~smooth.spline(.x[[ !!date_name ]], .x$roll_mean)),
-      first_deriv_ma = purrr::map2(.data$spline_ma, .data$data, ~{
-        predict(.x, .y$date_numeric, deriv = 1) %>%
-          tibble::as_tibble()
-      }),
-      # second_deriv_ma = purrr::map2(.data$spline_ma, .data$data, ~{
-      #   predict(.x, .y$date_numeric, deriv = 2) %>%
-      #
-      #     tibble::as_tibble()
-      # }),
       data = purrr::pmap(
         .l = list(
           .data$data,
-          .data$first_deriv,
-          .data$first_deriv_ma
-          # .data$second_deriv_ma
+          .data$first_deriv
         ),
         .f = ~{
 
           out <- dplyr::bind_cols(
             ..1,
-            dplyr::select(..2, first_deriv = y),
-            dplyr::select(..3, first_deriv_ma = y)
-            # dplyr::select(..3, second_deriv_ma = y)
+            dplyr::select(..2, first_deriv = y)
           )
 
           return(out)
@@ -80,15 +98,16 @@ rolling_slope <- function(data, date_col, value_col, ..., rolling_window = 8, re
   if (return_models) return(output)
 
   output <- output %>%
-    dplyr::select(!!!group_cols, data) %>%
+    dplyr::select({{group_col}}, data) %>%
     tidyr::unnest(data)
+
 
   return(output)
 
 }
 
 
-rolling_mean <- function(value, date_col, window){
-  # slider::slide_dbl(.x = value, .f = mean, .before = window, na.rm = TRUE)
-  slider::slide_index_dbl(.x = value, .i = date_col, .f = mean, .before = lubridate::weeks(window), na.rm = TRUE)
-}
+# rolling_mean <- function(value, date_col, window){
+#   # slider::slide_dbl(.x = value, .f = mean, .before = window, na.rm = TRUE)
+#   slider::slide_index_dbl(.x = value, .i = date_col, .f = mean, .before = lubridate::weeks(window), na.rm = TRUE)
+# }
